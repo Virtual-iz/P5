547b57cef83ce9c3ba4ff66668361867b90cbb18
Isazez <isazez@etik.com>
2025-12-17 22:02:15 +0100

    premier commit

--- src/components/ThumbContainer.jsx
+++ src/components/ThumbContainer.jsx
@@ -1,9 +1,15 @@
 import '../styles/ThumbContainer.scss'
+import Thumb from "../components/Thumb"
 
 const ThumbContainer = () => {
     return (
         <article className="thumb-container">
-            
+            <Thumb/>
+            <Thumb/>
+            <Thumb/>
+            <Thumb/>
+            <Thumb/>
+            <Thumb/>
         </article>
     )
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,18 +1,13 @@
 import Banner from "../components/Banner"
 import ThumbContainer from "../components/ThumbContainer"
-import Thumb from "../components/Thumb"
+
 
 const Home = () => {
     return (
     <main>
         <Banner />
         <ThumbContainer>
-            <Thumb/>
-            <Thumb/>
-            <Thumb/>
-            <Thumb/>
-            <Thumb/>
-            <Thumb/>
+
         </ThumbContainer>
     </main>
     )
